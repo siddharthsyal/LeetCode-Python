8dd178dd055e5f2171bc7f3fc04704dbf143160c
siddsyal <siddsyal@amazon.com>
2025-03-28 15:01:02 -0700

    solved for edge cases

--- Two Sum/twoSum.py
+++ Two Sum/twoSum.py
@@ -1,13 +1,12 @@
 from email.charset import add_codec
 from typing import List
 
-
 def twoSum(nums: List[int], target: int) -> List[int]:
     tracker = {}
     returnData: List[int] = []
     for i in range(len(nums)):
-        tracker[nums[i]] = i
         if (i == 0):
+            tracker[nums[i]] = i
             continue
         else:
             result = tracker.get(target - nums[i])
@@ -15,5 +14,6 @@ def twoSum(nums: List[int], target: int) -> List[int]:
                 returnData.append(result)
                 returnData.append(i)
                 return returnData
-
+            else:
+                tracker[nums[i]] = i
     return returnData
